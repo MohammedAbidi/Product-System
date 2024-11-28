c5e5f65246d1cd11aebb04c850b3372fef882177
Hector Dondiego <hdondiego@Hectors-MacBook-Pro.local>
2024-11-27 19:40:30 -0600

    Completed a majority of Shop interface and added endpoints in backend

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () => {
             </div>
             <ul className="nav-menu">
                 <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link>{menu === "shop" ? <hr/> : <></>}</li>
-                <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none'}} to='/mens'>Men</Link>{menu === "mens" ? <hr/> : <></>}</li>
-                <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none'}} to='/womens'>Women</Link>{menu === "womens" ? <hr/> : <></>}</li>
-                <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none'}} to='/kids'>Kids</Link>{menu === "kids" ? <hr/> : <></>}</li>
+                <li onClick={()=>{setMenu("fulfillment")}}><Link style={{ textDecoration: 'none'}} to='/fulfillment'>Fulfillment</Link>{menu === "fulfillment" ? <hr/> : <></>}</li>
+                <li onClick={()=>{setMenu("receiving")}}><Link style={{ textDecoration: 'none'}} to='/receiving'>Receiving</Link>{menu === "receiving" ? <hr/> : <></>}</li>
+                <li onClick={()=>{setMenu("administration")}}><Link style={{ textDecoration: 'none'}} to='/administration'>Administration</Link>{menu === "administration" ? <hr/> : <></>}</li>
             </ul>
             <div className='nav-login-cart'>
                 <Link to='/login'><button>Login</button></Link>
--- server/server.js
+++ server/server.js
@@ -6,23 +6,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = mariadb.createPool({
+const legacyPool = mariadb.createPool({
     host: 'blitz.cs.niu.edu',
     port: 3306,
     user: 'student',
     password: 'student',
     database: 'csci467',
 });
-//     connectionLimit: 5,
 
-// GET request
-app.get("/api/data/shop", async (req, res) => {
+// Sending the parts (excluding the available quantity) from the legacy DB
+app.get("/api/shop/items", async (req, res) => {
     let connection;
     try {
-        connection = await pool.getConnection();
-        console.log(connection);
+        connection = await legacyPool.getConnection();
         const rows = await connection.query("SELECT * FROM parts");
-        //console.log(rows);
         res.json(rows);
     } catch (error) {
         res.status(500).send({ error: 'Database query failed' });
@@ -33,7 +30,32 @@ app.get("/api/data/shop", async (req, res) => {
     }
 });
 
-app.post('/api/pay', async (req, res) => {
+// Sending a dictionary of the available quantities for each part
+app.get("/api/shop/quantities", async (req, res) => {
+    console.log("Sending available quantities for each item");
+});
+
+/*
+    Reading the billing information and the items to be purchased,
+    and if the transaction is successful, returns the following info:
+    - Order ID (for the order number created)
+    - Amount
+    - Authorization code
+    - Name
+    - Email
+*/
+app.post('/api/shop/pay', async (req, res) => {
+    /*
+        name: string,
+        email: string,
+        address: string,
+        creditCard: number,
+        expiration: string,
+        orderItems: [ 1, 2 ],
+        quantities: [ 3, 3 ],
+        amount: number,
+        shipping: number
+    */
     let paymentInfo = req.body;
     console.log(paymentInfo);
 
@@ -43,9 +65,11 @@ app.post('/api/pay', async (req, res) => {
         cc: paymentInfo.creditCard,
         name: paymentInfo.name,
         exp: paymentInfo.expiration,
-        amount: "675.33"
+        amount: paymentInfo.amount
     };
 
+    // Temporarily commenting out the below code until ready for output
+    /*
     fetch("https://blitz.cs.niu.edu/CreditCard", {
         method: "POST",
         headers: {
@@ -55,7 +79,6 @@ app.post('/api/pay', async (req, res) => {
     })
     .then(response => response.json())
     .then(data => {
-        // console.log("Success: ", data); // this will always be returned even if the payment failed (it will also contain the "errors" key)
         if ("errors" in data) {
             // if payment failed - the send() message won't show either way so maybe remove that
             //res.status(402).send( {"status" : "DECLINED"} )
@@ -68,6 +91,42 @@ app.post('/api/pay', async (req, res) => {
     .catch(error => {
         console.error("Error: ", error);
     });
+    */
+    const output = {
+        orderId: 12,
+        amount: 221.56,
+        authorization: 14444,
+        name: "John Doe",
+        email: "jdoe@pizza.net"
+    };
+
+    res.status(200).json(output);
+});
+
+// Sending a JSON array of orders - need to find a way to handle combining Customers, Orders, and Order_Items data together
+app.get("/api/orders", async (req, res) => {
+    console.log("Sending the list of orders");
+});
+
+// Updating the order to be marked as filled and, based on the filled order's items,
+// deducting each ordered item's available quantity by 1
+app.post("/api/ff/complete", async (req, res) => {
+    console.log("Updating the order row and item available row");
+});
+
+// Updating a row from the quantity table to reflect the item's new available quantity 
+app.post("/api/rcv/available", async (req, res) => {
+    console.log("Updating a row from the quantity table to reflect the new quantity on hand");
+});
+
+// Sending all of the rows from the backet table
+app.get("/api/admin/brackets", async (req, res) => {
+    console.log("Sending the bracket table's rows");
+});
+
+// Adding and updating rows in brackets table for new ranges
+app.post("/api/admin/add_bracket", async (req, res) => {
+    console.log("Adding and updating rows in brackets table for new ranges");
 });
 
 const PORT = 5000;
