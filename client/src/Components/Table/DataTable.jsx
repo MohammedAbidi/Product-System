87e968ca42a94a3b02a108bbfb2282216a2d95b7
Hector Dondiego <hdondiego@Hectors-MacBook-Pro.local>
2024-11-28 21:19:13 -0600

    Completed frontend aspect of Shop page

--- client/src/Components/Table/DataTable.jsx
+++ client/src/Components/Table/DataTable.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOM from "react-dom";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import './DataTable.css'
 
 function DataTable() {
@@ -14,32 +16,106 @@ function DataTable() {
     var [shippingCost, setShippingCost] = useState(0.0);
     var [total, setTotal] = useState(0.0);
 
-    // Retrieving the quantities
+    var [confirmedOrder, setConfirmedOrder] = useState({});
+    const [openConfirmation, setOpenConfirmation] = useState(false);
+    const [openDeclined, setOpenDeclined] = useState(false);
+    const [dialogTitle, setDialogTitle] = useState("");
+    const [dialogContent, setDialogContent] = useState("");
+
     useEffect(() => {
-        // Initialize an empty object
-        const tempAvailable = {};
-    
-        // Replace this block of code with the fetch lines of code
-        // Populate the object with keys from 1 to 149 and random values between 0 and 50
-        for (let i = 1; i <= 149; i++) {
-            tempAvailable[i] = Math.floor(Math.random() * 51); // Generates a random integer between 0 and 50
+        if (confirmedOrder && Object.keys(confirmedOrder).length > 0) {
+            let dialogTitle = "Confirmation: Order " + confirmedOrder["orderId"] + " created";
+            let dialogContent = "Amount: $" + confirmedOrder["amount"].toFixed(2) + 
+                                " Auth: " + confirmedOrder["authorization"] + "\n" + 
+                                "For: " + confirmedOrder["name"] + ", " + confirmedOrder["email"];
+            setDialogTitle(dialogTitle);
+            setDialogContent(dialogContent);
+            setOpenConfirmation(true);
         }
-    
-        // Update the available state once
-        setAvailable(tempAvailable);
-    }, []); // The empty dependency array ensures this runs only once
+    }, [confirmedOrder]);  
+    /* 
+    const handleClose = () => {
+        setCartItems([]);
+        setItemQuantities([]);
+        setAmount(0.0);
+        setWeight(0.0);
+        setShippingCost(0.0);
+        setTotal(0.0);
+        setConfirmedOrder({});
+        setDialogTitle("");
+        setDialogContent("");
+
+        const nameField = document.getElementById('name-field');
+        const emailField = document.getElementById('email-field');
+        const addressField = document.getElementById('address-field');
+        const creditCardField = document.getElementById('cc-field');
+        const expirationField = document.getElementById('exp-field');
+
+        nameField.value = "";
+        emailField.value = "";
+        addressField.value = "";
+        creditCardField.value = "";
+        expirationField.value = "";
+
+        updateCart();
+        setOpen(false);
+    };*/
+    const handleCloseConfirmation = () => {
+        setCartItems([]);
+        setItemQuantities([]);
+        setAmount(0.0);
+        setWeight(0.0);
+        setShippingCost(0.0);
+        setTotal(0.0);
+        setConfirmedOrder({});
+        setDialogTitle("");
+        setDialogContent("");
+
+        document.getElementById('name-field').value = "";
+        document.getElementById('email-field').value = "";
+        document.getElementById('address-field').value = "";
+        document.getElementById('cc-field').value = "";
+        document.getElementById('exp-field').value = "";
+        setOpenConfirmation(false);
+    };
+
+    const handleCloseDeclined = () => {
+        setOpenDeclined(false);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/shop/quantities');
+                const result = await response.json();
+                setAvailable(result);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
     
     // Retrieving the Shipping details
     useEffect(() => {
         // Shipping table is called "Brackets"
-        const tempShippingRanges = [
-            { "id": 1, "low": 0, "high": 5, "price": 0.0 },
-            { "id": 2, "low": 5, "high": 10, "price": 5.0 },
-            { "id": 3, "low": 10, "high": 15, "price": 10.0 },
-            { "id": 4, "low": 15, "high": 20, "price": 15.0}
-        ];
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/admin/brackets');
+                const result = await response.json();
+                setShippingRanges(result);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-        setShippingRanges(tempShippingRanges);
+        fetchData();
+        //setShippingRanges(tempShippingRanges);
     }, []);
 
     // Function to determine the shipping price
@@ -107,146 +183,29 @@ function DataTable() {
 
         updateCart();
     }
-
+    
     function updateCart() {
-        const numItemsMsg = document.getElementById("num-items-msg");
-        const itemList = document.getElementById("item-list"); // Table body
-
-        itemList.innerHTML = ""; // clearing all rows from the list of items shown in cart
-
-        setAmount(0.0);
-        setWeight(0.0);
-        setShippingCost(0.0);
-        setTotal(0.0);
-
-        let detailsSection = document.getElementById("details");
-
-        if (cartItems.length === 0) {
-            numItemsMsg.textContent = "You have " + cartItems.length + " items in your cart.";
-            detailsSection.style.display = "none";
-        } else if (cartItems.length === 1) {
-            // there is one item in the cart
-            numItemsMsg.textContent = "You have " + cartItems.length + " item in your cart.";
-
-            detailsSection.style.display = "block";
-
-            let itemId = cartItems[0]; // since there is only one item in the cart
-            let product = data.find(item => {
-                return item.number === itemId;
-            })
-
-            let img = document.createElement("img");
-            img.src = product.pictureURL;
-            img.style = "display: inline-block";
-
-            const text = document.createElement("div");
-            text.style = "display: inline-block"
-            let descText = document.createTextNode(product.description);
-            let weightText = document.createTextNode("Weight: " + product.weight + "lbs");
-            let priceText = document.createTextNode("Price: $" + product.price);
-            let quantityText = document.createTextNode("Qty: " + itemQuantities[0]);
-            text.appendChild(descText);
-            text.appendChild(document.createElement("br"));
-            text.appendChild(weightText);
-            text.appendChild(document.createElement("br"));
-            text.appendChild(priceText);
-            text.appendChild(document.createElement("br"));
-            text.appendChild(quantityText);
-
-            let removeBtn = document.createElement("button")
-            removeBtn.textContent = "Remove";
-            removeBtn.onclick = function () {
-                removeFromCart(product);
-            };
-
-            let buttonContainer = document.createElement("div");
-            buttonContainer.className = "center-vertically";
-            buttonContainer.appendChild(removeBtn);
-
-            let newRow = itemList.insertRow();
-
-            let newCellLeft = newRow.insertCell();
-            newCellLeft.appendChild(img);
-            newCellLeft.appendChild(text);
-
-            let newCellRight = newRow.insertCell();
-            newCellRight.appendChild(buttonContainer);
-
-            // Updating the Cart information
-            let tempAmount = product.price * itemQuantities[0];
-            let tempWeight = product.weight * itemQuantities[0]; // use this for calculating the shipping cost
-            let tempShippingCost = calculateShipping(tempWeight); // will need to fix this once Shipping db table is ready
-            let tempTotal = tempAmount + tempShippingCost;
-
-            setAmount(tempAmount);
-            setWeight(tempWeight);
-            setShippingCost(tempShippingCost);
-            setTotal(tempTotal);
-        } else if (cartItems.length > 1) {
-            numItemsMsg.textContent = "You have " + cartItems.length + " items in your cart.";
-
-            detailsSection.style.display = "block";
-
-            // Updating the Cart information
-            let tempAmount = 0.0;
-            let tempWeight = 0.0; // use this for calculating the shipping cost
-            let tempShippingCost = 0.0; // will need to fix this once Shipping db table is ready
-            let tempTotal = 0.0;
-            for (let i = 0; i < cartItems.length; i++) {
-                let itemId = cartItems[i];
-                let product = data.find(item => {
-                    return item.number === itemId;
-                })
-
-                let img = document.createElement("img");
-                img.src = product.pictureURL;
-                img.style = "display: inline-block";
-
-                const text = document.createElement("div");
-                text.style = "display: inline-block"
-                let descText = document.createTextNode(product.description);
-                let weightText = document.createTextNode("Weight: " + product.weight + "lbs");
-                let priceText = document.createTextNode("Price: $" + product.price);
-                let quantityText = document.createTextNode("Qty: " + itemQuantities[i]);
-                text.appendChild(descText);
-                text.appendChild(document.createElement("br"));
-                text.appendChild(weightText);
-                text.appendChild(document.createElement("br"));
-                text.appendChild(priceText);
-                text.appendChild(document.createElement("br"));
-                text.appendChild(quantityText);
-
-                let removeBtn = document.createElement("button")
-                removeBtn.textContent = "Remove";
-                removeBtn.onclick = function () {
-                    removeFromCart(product);
-                };
-
-                let buttonContainer = document.createElement("div");
-                buttonContainer.className = "center-vertically";
-                buttonContainer.appendChild(removeBtn);
-
-                let newRow = itemList.insertRow();
-
-                let newCellLeft = newRow.insertCell();
-                newCellLeft.appendChild(img);
-                newCellLeft.appendChild(text);
-
-                let newCellRight = newRow.insertCell();
-                newCellRight.appendChild(buttonContainer);
-
-                // Updating the Cart information
-                tempAmount += product.price * itemQuantities[i];
-                tempWeight += product.weight * itemQuantities[i]; // use this for calculating the shipping cost
-            }
-            tempShippingCost = calculateShipping(tempWeight); // will need to fix this once Shipping db table is ready
-            tempTotal = tempAmount + tempShippingCost;
-
-            setAmount(tempAmount);
-            setWeight(tempWeight);
-            setShippingCost(tempShippingCost);
-            setTotal(tempTotal);
-        }
+        const numItemsMsg = cartItems.length === 1 
+            ? `You have 1 item in your cart.` 
+            : `You have ${cartItems.length} items in your cart.`;
+    
+        const tempAmount = cartItems.reduce((acc, item, index) => {
+            const product = data.find(prod => prod.number === item);
+            return acc + product.price * itemQuantities[index];
+        }, 0);
+    
+        const tempWeight = cartItems.reduce((acc, item, index) => {
+            const product = data.find(prod => prod.number === item);
+            return acc + product.weight * itemQuantities[index];
+        }, 0);
+    
+        const tempShippingCost = calculateShipping(tempWeight);
+        const tempTotal = tempAmount + tempShippingCost;
+    
+        setAmount(tempAmount);
+        setWeight(tempWeight);
+        setShippingCost(tempShippingCost);
+        setTotal(tempTotal);
     }
 
     const handlePaymentSubmit = async (event) => {
@@ -281,8 +240,12 @@ function DataTable() {
             if (response.ok) {
                 const result = await response.json();
                 console.log('Payment success:', result);
+
+                setConfirmedOrder(result);
+                //handleClickOpen();
             } else {
                 console.error('Payment failed:', response.statusText);
+                setOpenDeclined(true);
             }
         } catch (error) {
             console.error('Error submitting payment:', error);
@@ -317,7 +280,6 @@ function DataTable() {
                     <thead>
                         <tr>
                             <th>PictureURL</th>
-                            <th>Number</th>
                             <th>Description</th>
                             <th>Price</th>
                             <th>Weight</th>
@@ -330,7 +292,6 @@ function DataTable() {
                                 <td>
                                     <img src={item.pictureURL} alt={item.description} />
                                 </td>
-                                <td>{item.number}</td>
                                 <td>{item.description}</td>
                                 <td>{"$" + (item.price).toFixed(2)}</td>
                                 <td>{item.weight}</td>
@@ -352,9 +313,28 @@ function DataTable() {
                 <h1>Cart</h1>
                 <p id='num-items-msg'>You have {cartItems.length} items in your cart.</p>
                 <br/>
-                <div id='details'>
+                <div id='details' style={{ 
+                    display: (cartItems.length === 0) ? "none" : "block" }} >
                     <table id='cart-products'>
                         <tbody id='item-list'>
+                            {data.filter(item => cartItems.includes(item.number)).map((item) => (
+                                <tr key={item.number}>
+                                    <td style={{ display: "inline-block", padding: "1em"}}>
+                                        <img src={item.pictureURL} alt={item.description} />
+                                    </td>
+                                    <td style={{ display: "inline-block", padding: "1em"}}>
+                                        <div>
+                                            <p>{item.description}</p>
+                                            <p>{("Weight: " + (item.weight).toFixed(2) + "lbs")}</p>
+                                            <p>{("Price: $" + (item.price).toFixed(2))}</p>
+                                            <p>{`Qty: ${itemQuantities[cartItems.indexOf(item.number)] || 0 }`}</p>
+                                        </div>
+                                    </td>
+                                    <td style={{ display: "inline-block", padding: "1em"}}>
+                                        <button onClick={() => removeFromCart(item)}>Remove</button>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                     <br/>
@@ -374,7 +354,7 @@ function DataTable() {
                                     <b>Weight: </b>
                                 </td>
                                 <td>
-                                    <p id='weight'>{(weight) + "lbs"}</p>
+                                    <p id='weight'>{(weight).toFixed(2) + "lbs"}</p>
                                 </td>
                             </tr>
 
@@ -448,10 +428,36 @@ function DataTable() {
                     </form>
                 </div>
             </div>
+
+            <Dialog open={openConfirmation} onClose={handleCloseConfirmation}>
+                <DialogTitle>{dialogTitle}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {dialogContent}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseConfirmation} color="primary" variant="outlined">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={openDeclined} onClose={handleCloseDeclined}>
+                <DialogTitle>Transaction Failed</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please enter valid billing information.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDeclined} color="primary" variant="outlined">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>    
     );
 };
-
-//                                        <input id='quantity' type='number' min='0' placeholder='0' style={{display: "inline-flex", width: "5em"}}/>
 
 export default DataTable
