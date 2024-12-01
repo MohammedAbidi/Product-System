46ba8e5ca21946b5c6a75764e636a816f23a922a
MohammedAbidi <mhabidi313@gmail.com>
2024-12-01 10:48:49 -0600

    Seems like a good idea idk
    Added secrets.js for the new server.js

---
+++ server/secrets.js
@@ -0,0 +1,8 @@
+// This information probably shouldn't be public on Github like this
+module.exports = Object.freeze({
+    LEGACY_HOST: 'blitz.cs.niu.edu',
+    LEGACY_PORT: 3306,
+    LEGACY_USER: 'student',
+    LEGACY_PASSWORD: 'student',
+    LEGACY_DATABASE: 'csci467',
+});
