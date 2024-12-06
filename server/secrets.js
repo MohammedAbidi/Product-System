38f65b5a0b6a9849ff43c631f99761b33f1d2b36
MohammedAbidi <mhabidi313@gmail.com>
2024-12-05 23:30:24 -0600

    Switched to postgres (for Vercel)

--- server/secrets.js
+++ server/secrets.js
@@ -3,12 +3,13 @@ module.exports = Object.freeze({
     VENDOR_ID: "VE001-99", // Is this our store id?
     TRANSACTION_ID: "907-998654333-296", // Our bank id?
     CC_WEBSITE: "https://blitz.cs.niu.edu/CreditCard",
+    PORT: 5000,
 
-    LOCAL_HOST: "localhost",
-    LOCAL_PORT: 5000,
-    LOCAL_USER: "root",
-    LOCAL_PASSWORD: "password",
-    LOCAL_DATABASE: "ProductDB",
+    DB_HOST: "local_host",
+    DB_PORT: 5432,
+    DB_USER: "postgres", // root
+    DB_PASSWORD: "password",
+    DB_DATABASE: "productdb",
 
     LEGACY_HOST: 'blitz.cs.niu.edu',
     LEGACY_PORT: 3306,
