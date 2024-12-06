babddc9b83df4da44b1c309337a7752c533fd0f5
MohammedAbidi <mhabidi313@gmail.com>
2024-12-06 11:52:52 -0600

    please work

--- server/routes/db.router.js
+++ server/routes/db.router.js
@@ -3,14 +3,14 @@ const router = express.Router()
 
 const dbController = require('../controllers/db.controller')
 
-router.get("/shop/quantities", dbController.nothing)
-router.post("/shop/pay", dbController.nothing)
+router.get("/shop/quantities", dbController.get_shop_quantities)
+router.post("/shop/pay", dbController.post_shop_pay)
 
-router.get("/orders", dbController.nothing)
-router.post("/ff/complete", dbController.nothing)
+router.get("/orders", dbController.get_orders)
+router.post("/ff/complete", dbController.post_ff_complete)
 
-router.post("/rcv/available", dbController.nothing)
-router.get("/admin/brackets", dbController.nothing)
-router.post("/admin/add_bracket", dbController.nothing)
+router.post("/rcv/available", dbController.post_rcv_available)
+router.get("/admin/brackets", dbController.get_admin_brackets)
+router.post("/admin/add_bracket", dbController.post_admin_add_bracket)
 
 module.exports = router
