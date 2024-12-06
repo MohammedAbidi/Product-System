e67040e655e9cd4861aede8ed4b83256cf3af0d3
MohammedAbidi <mhabidi313@gmail.com>
2024-12-06 12:37:31 -0600

    typo

--- client/src/Components/Table/ReceivingTable.jsx
+++ client/src/Components/Table/ReceivingTable.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
 import './ReceivingTable.css'
 
-// const WEBSITE = "https://cumbersome-mountainous-jackfruit.glitch.me/";
-const WEBSITE = "https://productsystemdb.vercel.app/";
+// const WEBSITE = "https://cumbersome-mountainous-jackfruit.glitch.me";
+const WEBSITE = "https://productsystemdb.vercel.app";
 // const WEBSITE = "http://localhost:5000";
 
 function DataTable() {
